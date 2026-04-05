735aba83196ffb3aa1f0f0f469d14aa7ba42a31b
JE Webmarketing <contact@je-webmarketing.com>
2026-04-05 21:11:43 +0200

    force firebase render json

--- services/firebaseAdmin.js
+++ services/firebaseAdmin.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
 
 function getServiceAccount() {
   const jsonEnv = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
@@ -15,18 +13,6 @@ function getServiceAccount() {
   console.log("✅ Firebase chargé depuis variable Render");
 
   return JSON.parse(jsonEnv);
-}
-  if (filePath) {
-    const fullPath = path.resolve(filePath);
-    console.log("📁 Firebase service account chargé depuis fichier :", fullPath);
-
-    const file = fs.readFileSync(fullPath, "utf-8");
-    return JSON.parse(file);
-  }
-
-  throw new Error(
-    "Firebase non configuré : ajoute FIREBASE_SERVICE_ACCOUNT_JSON ou FIREBASE_SERVICE_ACCOUNT_FILE"
-  );
 }
 
 const serviceAccount = getServiceAccount();
